AITester - Finished tester found and fixed several bugs:  - Added Builder for input functions.  - Fixed problem with tf columns having nan in vocabulary list.  - Gradient clipping issue with NoneSpecification.  - Added callable spec.  - Added scrubber for converting string to a datetime.

AI.py
@@ -27,11 +27,19 @@ class AbstractAI(ABC):
         pass
 
     @abstractmethod
+    def get_training_data(self):
+        pass
+
+    @abstractmethod
     def set_training_fn(self, input_fn: Callable):
         pass
 
     @abstractmethod
     def set_evaluation_data(self, validation_data: DataModel):
+        pass
+
+    @abstractmethod
+    def get_evaluation_data(self):
         pass
 
     @abstractmethod
@@ -48,7 +56,6 @@ class AbstractAI(ABC):
 
 
 class AI(AbstractAI):
-
     training_data: DataModel
     training_fn: Callable
     evaluation_data: DataModel
@@ -56,17 +63,23 @@ class AI(AbstractAI):
     estimator: tf.estimator
     optimizer: tf.train.Optimizer
 
+    def __init__(self):
+        super().__init__()
+        self.training_data = None
+        self.evaluation_data = None
+
     def train(self):
-        if type(self.training_fn) is not Callable:
-            raise RuntimeError('Training input Callable not set on AI: {}'.format(self.__class__.__name__))
+        assert callable(self.training_fn), 'Training input Callable not set on AI: {}'\
+            .format(self.__class__.__name__)
 
         self.estimator.train(input_fn=self.training_fn)
 
     def evaluate(self, model_dir: str) -> dict:
-        if type(self.evaluation_fn) is not Callable:
-            raise RuntimeError('Evaluation input Callable not set on AI: {}'.format(self.__class__.__name__))
+        assert callable(self.evaluation_fn), 'Evaluation input Callable not set on AI: {}'\
+            .format(self.__class__.__name__)
 
         self.estimator.model_dir = model_dir
+
         return self.estimator.evaluate(input_fn=self.evaluation_fn)
 
     def estimate(self):
@@ -78,8 +91,14 @@ class AI(AbstractAI):
     def set_training_fn(self, input_fn: Callable):
         self.training_fn = input_fn
 
-    def set_evaluation_data(self, validation_data: DataModel):
-        self.evaluation_data = validation_data
+    def get_training_data(self):
+        return self.evaluation_data
+
+    def set_evaluation_data(self, evaluation_data: DataModel):
+        self.evaluation_data = evaluation_data
+
+    def get_evaluation_data(self):
+        return self.evaluation_data
 
     def set_evaluation_fn(self, input_fn: Callable):
         self.evaluation_fn = input_fn

AIFactory/Builders/DataBuilder.py
@@ -15,9 +15,11 @@ class DataBuilder(Builder):
     def __init__(self, data_source: str,
                  target_column: str,
                  validation_data_percentage: int,
-                 feature_columns: list,
+                 feature_columns: dict,
+                 data_columns: list,
                  metadata: MetaData):
 
+        self.data_columns = data_columns
         self.metadata = metadata
         self.validation_data_percentage = RangeSpecification('validation_data_perc', validation_data_percentage, 0, 100)
         self.training_data_percentage = RangeSpecification(name='training_data_perc',
@@ -31,8 +33,9 @@ class DataBuilder(Builder):
         self.test_data = None
         self.validation_data = None
 
-        for feature_column in feature_columns:
-            self.add_feature_column(name=feature_column[0], column_type=feature_column[1])
+        # validate input.
+        for name, type in feature_columns.items():
+            self.add_feature_column(name=name, column_type=type)
 
     @property
     def dependent_on(self) -> list:
@@ -59,7 +62,7 @@ class DataBuilder(Builder):
         data.set_tf_feature_columns(feature_columns)
 
         split_data = self.split_validation_and_test_data(data=data)
-        ai.set_validation_data(split_data['validation_data'])
+        ai.set_evaluation_data(split_data['validation_data'])
         ai.set_training_data(split_data['training_data'])
 
     def load_data(self) -> DataModel:
@@ -69,7 +72,7 @@ class DataBuilder(Builder):
 
         columns = self.get_feature_column_names()
         columns.append(self.target_column())
-
+        columns = columns + self.data_columns
         loader.filter_columns(columns)
 
         return loader.get_dataset()
@@ -121,9 +124,14 @@ class DataBuilder(Builder):
             data: DataModel
     ) -> tf.feature_column.categorical_column_with_vocabulary_list:
 
+        categories = data.get_all_column_categories(feature_column_data['name'])
+
+        # todo: refactor so tf columns are manufactured in different builder after scrubbing.
+        filtered_categories = [cat for cat in categories if type(cat) is str]
+
         return tf.feature_column.categorical_column_with_vocabulary_list(
             feature_column_data['name'],
-            vocabulary_list=data.get_all_column_categories(feature_column_data['name'])
+            vocabulary_list=filtered_categories
         )
 
 
@@ -133,7 +141,7 @@ class TestDataBuilder(unittest.TestCase):
         data_builder = DataBuilder(data_source='../../../data/test_data.csv',
                                    target_column='target_1',
                                    validation_data_percentage=20,
-                                   feature_columns=[],
+                                   feature_columns={},
                                    metadata=MetaData())
 
         data_builder.add_feature_column(name='feature_1', column_type=DataBuilder.CATEGORICAL_COLUMN_VOC_LIST)
@@ -147,7 +155,26 @@ class TestDataBuilder(unittest.TestCase):
 
         feature_names = ['feature_1', 'feature_2', 'feature_3']
         self.validate_data_frame(arti.training_data, feature_names)
-        self.validate_data_frame(arti.validation_data, feature_names)
+        self.validate_data_frame(arti.evaluation_data, feature_names)
+
+    def test_constructor_build(self):
+        data_builder = DataBuilder(data_source='../../../data/test_data.csv',
+                                   target_column='target_1',
+                                   validation_data_percentage=20,
+                                   feature_columns={
+                                        'feature_1': DataBuilder.CATEGORICAL_COLUMN_VOC_LIST,
+                                        'feature_2': DataBuilder.NUMERICAL_COLUMN,
+                                        'feature_3': DataBuilder.NUMERICAL_COLUMN
+                                   },
+                                   metadata=MetaData())
+
+        arti = AI()
+        data_builder.validate()
+        data_builder.build(ai=arti)
+
+        feature_names = ['feature_1', 'feature_2', 'feature_3']
+        self.validate_data_frame(arti.training_data, feature_names)
+        self.validate_data_frame(arti.evaluation_data, feature_names)
 
     def validate_data_frame(self, data_frame: DataModel, feature_name_list: list):
         self.assertEqual(data_frame.feature_columns_names, feature_name_list)

AIFactory/Builders/OptimizerBuilder.py
@@ -38,8 +38,8 @@ class OptimizerBuilder(Builder):
     def build(self, neural_net: AbstractAI):
         my_optimizer = self._set_optimizer(optimizer_type=self.optimizer_type(), learning_rate=self.learning_rate())
 
-        if self.gradient_clipping is not None:
-            my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, self.gradient_clipping)
+        if self.gradient_clipping() is not None:
+            my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, self.gradient_clipping())
 
         neural_net.set_optimizer(my_optimizer)
 

InputFunctionHolder.py
@@ -15,18 +15,18 @@ def _dataset_to_dict(features):
     return {key: np.array(value) for key, value in dict(features).items()}
 
 
-def base_input_function(ml_dataset: Data.DataModel, batch_size=1, epoch=1):
+def base_fn(data_model: Data.DataModel, batch_size=1, epoch=1):
     """ input function one, made for shoes AI.
 
-    :param ml_dataset: Data.MLDataset
+    :param data_model: Data.MLDataset
     :param epoch: int
     :param batch_size: int
     :return:
     """
 
-    features = _dataset_to_dict(features=ml_dataset.get_feature_columns())
+    features = _dataset_to_dict(features=data_model.get_feature_columns())
 
-    data_set = tf.data.Dataset.from_tensor_slices((features, ml_dataset.get_target_column()))
+    data_set = tf.data.Dataset.from_tensor_slices((features, data_model.get_target_column()))
 
     data_set = data_set.shuffle(100).repeat(epoch).batch(batch_size)
 
@@ -56,7 +56,7 @@ class TestBaseInputFunction(unittest.TestCase):
 
     def test_feature_columns(self):
         self.setUp()
-        iterator = base_input_function(self._dataset)
+        iterator = base_fn(self._dataset)
         session = tf.Session()
         result1 = session.run(iterator)
 