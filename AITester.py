4f5101a5b25224c4282ff789ea9d6d15ea554f82
Jos88 <13717381+jos1988@users.noreply.github.com>
2018-12-02 21:12:40 +0100

    printer: Refactored printer code to printer in AITester module.

--- AITester.py
+++ AITester.py
@@ -3,6 +3,7 @@ from AIBuilder.AI import AbstractAI
 from AIBuilder.Summizer import Summizer
 from datetime import datetime
 import hashlib
+from _io import TextIOWrapper
 
 
 class AbstractAITester(ABC):
@@ -16,35 +17,46 @@ class AbstractAITester(ABC):
         pass
 
 
-# todo refactor write and print logic to writer and printer...
-
-
 class AITester(AbstractAITester):
     AI: AbstractAI
-    description_hash_label = '\n--- description hash: '
 
     def __init__(self, summizer: Summizer):
         self.summizer = summizer
         self.test_time = None
         self.results = {}
+        console_strategy = ConsolePrintStrategy()
+        self.console_printer = Printer(console_strategy)
+        self.report_printer = self.set_report_printer()
+        self.description_hash = None
 
     def run_AI_test(self, ai: AbstractAI):
         self.set_AI(ai)
+        self.description_hash = self.stable_hash_description(ai.description)
+
         if self.is_unique():
             self.train_AI()
             self.evaluate_AI()
         else:
-            print('AI evaluated!')
-            print(ai.description)
+            self.console_printer.line('')
+            self.console_printer.separate()
+            self.console_printer.line('AI already evaluated')
+            self.print_description()
+
+            self.console_printer.separate()
+
+    def set_report_printer(self):
+        report = self.open_report_file('a')
+        report_print_strategy = ReportPrintStrategy(report=report)
+
+        return Printer(report_print_strategy)
 
     def set_AI(self, ai: AbstractAI):
         self.AI = ai
 
     def is_unique(self) -> bool:
         report = self.open_report_file('r')
-        description_hash = AITester.stable_hash_description(self.AI.description)
         for line in report:
-            if -1 is not line.find(str(description_hash)):
+            if -1 is not line.find(str(self.description_hash)):
                 return False
 
         return True
@@ -57,67 +69,32 @@ class AITester(AbstractAITester):
 
     def evaluate_AI(self):
         self.summizer.log('start evaluation', None)
-
         self.results = self.AI.evaluate()
 
-        print()
         self.print_description()
-        print()
-        self.print_evaluation_results()
+        self.print_results()
         self.log_testing_report()
 
         self.summizer.log('finished evaluation', None)
         self.summizer.summize()
 
-    def print_evaluation_results(self):
+    def print_results(self):
+        self.console_printer.separate()
         self.validate_results_set()
-        for label, value in self.results.items():
-            print(label + ': ' + str(value))
+        self.console_printer.print_results(self.results)
 
     def print_description(self):
-        self.validate_test_time()
-        print('--- AI: ' + self.AI.get_name() + ' ---')
-        print('--- time: ' + self.test_time + ' ---')
-        for builder_name, description in self.AI.description.items():
-            print(builder_name)
-
-            if type(description) is not dict:
-                print(' - ' + description)
-                continue
-
-            for element, value in description.items():
-                print(' - ' + element + ' : ' + str(value))
+        self.console_printer.separate()
+        self.console_printer.print_ai_description(
+            ai=self.AI, time_stamp=self.test_time, ai_hash=self.description_hash)
 
     def log_testing_report(self):
-        self.validate_results_set()
-        self.validate_test_time()
-
-        report = self.open_report_file('a')
-        report.write('\n')
-        report.write('\n--- AI: ' + self.AI.get_name() + ' ---')
-        report.write('\n--- time: ' + self.test_time + ' ---')
-        self.write_description(report)
-        report.write('\n')
-        self.write_results(report)
-
-        report.close()
-
-    def write_results(self, report):
-        for label, value in self.results.items():
-            report.write('\n' + label + ': ' + str(value))
-
-    def write_description(self, report):
-        description_hash = AITester.stable_hash_description(self.AI.description)
-        report.write('\n--- description hash: ' + str(description_hash))
-        for builder_name, description in self.AI.description.items():
-            report.write('\n' + builder_name)
-
-            if type(description) is not dict:
-                report.write('\n - ' + description)
-                continue
-
-            for element, value in description.items():
-                report.write('\n - ' + element + ': ' + str(value))
+        self.report_printer.line('')
+        self.report_printer.print_ai_description(
+            ai=self.AI, time_stamp=self.test_time, ai_hash=self.description_hash)
+        self.report_printer.line('')
+        self.report_printer.print_results(self.results)
+        self.report_printer.separate()
 
     def validate_results_set(self):
         assert type(self.results) is dict, 'Test results not set in AI tester.'
@@ -140,3 +117,76 @@ class AITester(AbstractAITester):
         hash_result = hashlib.md5(description)
 
         return hash_result.hexdigest()
+
+
+class PrintStrategy(ABC):
+
+    @abstractmethod
+    def print(self, text: str):
+        pass
+
+    @abstractmethod
+    def new_line(self):
+        pass
+
+    def print_new_line(self, text: str):
+        self.new_line()
+        self.print(text)
+
+
+class ConsolePrintStrategy(PrintStrategy):
+
+    def print(self, text: str):
+        print(text)
+
+    def new_line(self):
+        print()
+
+
+class ReportPrintStrategy(PrintStrategy):
+
+    def __init__(self, report: TextIOWrapper):
+        self.report = report
+
+    def print(self, text: str):
+        self.report.write(text)
+
+    def new_line(self):
+        self.report.write('\n')
+
+    def close_report(self):
+        self.report.close()
+
+
+class Printer:
+
+    def __init__(self, strategy: PrintStrategy):
+        self.output = strategy
+
+    def separate(self,):
+        self.line('==================================================================================================')
+
+    def line(self, text):
+        self.output.print_new_line(text)
+
+    def print_ai_description(self, ai: AbstractAI, time_stamp: str = None, ai_hash: str = None):
+        self.line('--- AI: ' + ai.get_name() + ' ---')
+        if time_stamp is not None:
+            self.line('--- time: ' + time_stamp + ' ---')
+
+        if ai_hash is not None:
+            self.line('--- description hash: ' + str(ai_hash))
+
+        for builder_name, description in ai.description.items():
+            self.line(builder_name)
+
+            if type(description) is not dict:
+                self.line(' - ' + description)
+                continue
+
+            for element, value in description.items():
+                self.line(' - ' + element + ' : ' + str(value))
+
+    def print_results(self, results: dict):
+        for label, value in results:
+            self.line(label + ': ' + str(value))
